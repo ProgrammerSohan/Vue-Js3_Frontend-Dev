404baacc3cb89046104cdc3f45cd03eed89ead0d
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-15 17:53:41 +0600

    1.14_basic directives_data iteration with v-for

--- s1_ch1/first_app/src/App.vue
+++ s1_ch1/first_app/src/App.vue
@@ -9,6 +9,10 @@
     <h1>V-Model Part</h1>
     <br>
     <Exercise4 />
+    <br>
+    <h1>Data Iteration with v-for</h1>
+    <br>
+  <Exercise5 />
  
 </template>
 <script>
@@ -16,12 +20,14 @@ import Exercise from './components/Exercise1-01.vue'
 import Exercise2 from './components/Exercise1-02.vue'
 import Exercise3 from './components/Exercise1-03.vue'
 import Exercise4 from './components/Exercise1-04.vue'
+import Exercise5 from './components/Exercise1-05.vue'
 export default {
   components: {
     Exercise,
     Exercise2,
     Exercise3,
-    Exercise4
+    Exercise4,
+    Exercise5
   }
 
 }
