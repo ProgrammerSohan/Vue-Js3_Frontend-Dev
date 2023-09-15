e9b1a6c832b3ff1296441ab19d60a663f8f8b79a
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-15 16:50:32 +0600

    1.13_basic directives_v-model_f

--- s1_ch1/first_app/src/App.vue
+++ s1_ch1/first_app/src/App.vue
@@ -5,17 +5,23 @@
     <Exercise2 />
     <br>
     <Exercise3 />
+    <br>
+    <h1>V-Model Part</h1>
+    <br>
+    <Exercise4 />
  
 </template>
 <script>
 import Exercise from './components/Exercise1-01.vue'
 import Exercise2 from './components/Exercise1-02.vue'
 import Exercise3 from './components/Exercise1-03.vue'
+import Exercise4 from './components/Exercise1-04.vue'
 export default {
   components: {
     Exercise,
     Exercise2,
-    Exercise3
+    Exercise3,
+    Exercise4
   }
 
 }
