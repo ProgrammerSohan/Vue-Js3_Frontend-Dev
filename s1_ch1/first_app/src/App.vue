c6684762ada0cc3ddb7b88ca4eecbd10c9e3cc78
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-15 10:59:22 +0600

    1.4_exploring data properties as a local state

--- s1_ch1/first_app/src/App.vue
+++ s1_ch1/first_app/src/App.vue
@@ -1,18 +1,22 @@
 <template>
  
     <Exercise />
+    <br>
+    <Exercise2 />
  
 </template>
 <script>
 import Exercise from './components/Exercise1-01.vue'
+import Exercise2 from './components/Exercise1-02.vue'
 export default {
   components: {
     Exercise,
+    Exercise2
   }
 
 }
 
 </script>
-<style>
+<style> 
 
 </style>
---
+++ s1_ch1/first_app/src/components/Exercise1-02.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div>
+        <h1>{{title}}</h1>
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return {
+            title: 'This is Second Component!',
+        }
+    },
+
+}
+
+</script>
