e28a06ec21915070498d4823b0da1f35a8059f8f
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-15 18:14:47 +0600

    1.16_basic directives_data iterating through an array of objects-with v-for_p3

--- s1_ch1/first_app/src/components/Exercise1-05.vue
+++ s1_ch1/first_app/src/components/Exercise1-05.vue
@@ -10,7 +10,11 @@
             <li v-for="(item, index) in items" :key="item.id">
                 <h2>{{ item.title}}</h2>
                 <span>{{item.description}}</span>
-               
+               <ul>
+                <li v-for="(str, index) in item.characteristics" :key="index">
+                    <span>{{str}}</span>
+                </li>
+               </ul>
             </li>
         </ul>
     </div>
