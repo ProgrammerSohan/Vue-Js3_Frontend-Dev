fa29ae8d45a615ad9614f833a7f5c0fa7d21b8ca
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-15 12:59:38 +0600

    1.10_basic directives_v-bind_short syntax_c

--- s1_ch1/first_app/src/components/Exercise1-03.vue
+++ s1_ch1/first_app/src/components/Exercise1-03.vue
@@ -3,11 +3,24 @@
             <!-- <h1>{{ text }}</h1> -->
             <h1 v-once v-text="text"></h1>
             <h2 v-html="html"></h2>
+            <a 
+                :href="link.url"
+                :target="link.target"
+                :tabindex="link.tabindex"
+
+            >{{link.title}}</a>
     </div>
 </template>
 <script setup>
 const text = 'Directive text';
-const html = 'Stylise</br>HTML in<br/><b>your data</b>'
+const html = 'Stylise</br>HTML in<br/><b>your data</b>';
+const link = {
+    title: "Go to Youtube",
+    url: 'https://youtube.com',
+    tabindex: 1,
+    target: '_blank',
+
+};
 
 </script>
 <style>
