d8de4b5135bb39414ebebd4efbfe85d88a76e9b8
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-15 19:09:43 +0600

    1.17_basic directives_looping through array of objects with v-if conditions_p1

--- s1_ch1/first_app/src/App.vue
+++ s1_ch1/first_app/src/App.vue
@@ -13,6 +13,8 @@
     <h1>Data Iteration with v-for</h1>
     <br>
   <Exercise5 />
+  <br>
+  <Exercise6 />
  
 </template>
 <script>
@@ -21,13 +23,15 @@ import Exercise2 from './components/Exercise1-02.vue'
 import Exercise3 from './components/Exercise1-03.vue'
 import Exercise4 from './components/Exercise1-04.vue'
 import Exercise5 from './components/Exercise1-05.vue'
+import Exercise6 from './components/Exercise1-06.vue'
 export default {
   components: {
     Exercise,
     Exercise2,
     Exercise3,
     Exercise4,
-    Exercise5
+    Exercise5,
+    Exercise6
   }
 
 }
---
+++ s1_ch1/first_app/src/components/Exercise1-06.vue
@@ -0,0 +1,31 @@
+<template lang="">
+    <div>
+        <h1>Looping through array of objects with v-if conditions</h1>
+        <ul>
+            <li v-for="(item, n) in interests" :key="n">
+                {{item.title}}
+            </li>
+        </ul>
+    </div>
+</template>
+<script setup>
+ const interests = [
+    {
+        title: "TV",
+        favorites: ["Designed Survivor","Spongebob"],
+    },
+    {
+        title: "Games",
+        favorites: ["CS:GO"]
+    },
+    {
+        title: "Sports",
+        favorites: [],
+    },
+
+ ];
+
+</script>
+<style>
+
+</style>
