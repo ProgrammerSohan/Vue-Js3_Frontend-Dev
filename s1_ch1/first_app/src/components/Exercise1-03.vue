6a6bc8061186d78d79178d48e4b2cc473c1ae7b2
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-15 12:44:05 +0600

    1.9_basic directives_v-html_b

--- s1_ch1/first_app/src/components/Exercise1-03.vue
+++ s1_ch1/first_app/src/components/Exercise1-03.vue
@@ -2,10 +2,12 @@
     <div>
             <!-- <h1>{{ text }}</h1> -->
             <h1 v-once v-text="text"></h1>
+            <h2 v-html="html"></h2>
     </div>
 </template>
 <script setup>
 const text = 'Directive text';
+const html = 'Stylise</br>HTML in<br/><b>your data</b>'
 
 </script>
 <style>
