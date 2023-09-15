3c40365700f897456dcb035c3166a4513fd75ac6
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-15 12:22:26 +0600

    1.7_basic directives_0

--- s1_ch1/first_app/src/App.vue
+++ s1_ch1/first_app/src/App.vue
@@ -3,15 +3,19 @@
     <Exercise />
     <br>
     <Exercise2 />
+    <br>
+    <Exercise3 />
  
 </template>
 <script>
 import Exercise from './components/Exercise1-01.vue'
 import Exercise2 from './components/Exercise1-02.vue'
+import Exercise3 from './components/Exercise1-03.vue'
 export default {
   components: {
     Exercise,
-    Exercise2
+    Exercise2,
+    Exercise3
   }
 
 }
---
+++ s1_ch1/first_app/src/components/Exercise1-03.vue
@@ -0,0 +1,12 @@
+<template lang="">
+    <div>
+            <h1>{{ text }}</h1>
+    </div>
+</template>
+<script setup>
+const text = 'Directive text';
+
+</script>
+<style>
+
+</style>
