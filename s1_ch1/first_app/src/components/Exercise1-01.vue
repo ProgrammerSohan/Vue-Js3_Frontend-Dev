fb2c47c7c29620685dfb7ccfd38df1d6a69ce9fe
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-15 10:33:27 +0600

    1.3_building 1st component_c1

---
+++ s1_ch1/first_app/src/components/Exercise1-01.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>My First Component!</h1>
+    </div>
+</template>
+<script>
+export default {
+
+
+}
+
+</script>
+<style>
+h1{
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #0f7ff0;
+    margin-top: 60px;
+
+}
+
+</style>
