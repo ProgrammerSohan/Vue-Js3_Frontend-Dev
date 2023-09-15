4e075ffcce38744f52b0e20cb6a619c23f62cf54
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-15 12:28:22 +0600

    1.8_basic directives_a

--- s1_ch1/first_app/src/components/Exercise1-03.vue
+++ s1_ch1/first_app/src/components/Exercise1-03.vue
@@ -1,6 +1,7 @@
 <template lang="">
     <div>
-            <h1>{{ text }}</h1>
+            <!-- <h1>{{ text }}</h1> -->
+            <h1 v-once v-text="text"></h1>
     </div>
 </template>
 <script setup>
