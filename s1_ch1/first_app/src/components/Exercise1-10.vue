5936eef238f103415786e52569ff331f11a35d62
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-23 20:42:37 +0600

    1.31_beforeupdate() & updated()

--- s1_ch1/first_app/src/components/Exercise1-10.vue
+++ s1_ch1/first_app/src/components/Exercise1-10.vue
@@ -5,6 +5,7 @@
                 <li v-for="(item, n) in list" :key="n">
 
                     {{ item}}
+                    <button @click="deleteItem(item)">Delete</button>
                 </li>
             </ul>
 
@@ -35,13 +36,26 @@ export default {
     created(){
         alert('created: data and events ready, but no DOM');
     }*/
-
+/*
     beforeMount(){
         alert('beforeMount: $el not ready');
     },
     mounted(){
         alert('mounted: DOM ready to use');
 
+    },*/
+
+    methods:{
+        deleteItem(value){
+            this.list = this.list.filter(item=>item !== value)
+        },
+
+    },
+    beforeUpdate(){
+        alert('beforeUpdate: we know an update is about to happen, and have the data');
+    },
+    updated(){
+        alert('updated: virtual DOM will update after you click ok');
     },
 
 }
