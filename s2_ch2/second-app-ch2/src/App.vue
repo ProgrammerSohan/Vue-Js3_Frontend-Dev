81a22923b7cbd82448638eb7bf556911c461e176
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-08 07:14:22 +0600

    2.2_understanding computed properties

--- s2_ch2/second-app-ch2/src/App.vue
+++ s2_ch2/second-app-ch2/src/App.vue
@@ -1,15 +1,18 @@
 <template>
 
   <Test1 />
+  <br>
+  <Computedproperty0 />
 
 </template>
 <script>
   import Test1 from './components/Test1-01.vue'
+  import Computedproperty0 from './components/Computed-property0.vue'
 
 export default{
   components: {
     Test1,
-
+    Computedproperty0,
   }
   
 
---
+++ s2_ch2/second-app-ch2/src/components/Computed-property0.vue
@@ -0,0 +1,26 @@
+<template >
+    <input v-model="name">
+    <br>
+    <div>
+            <span>{{error}}</span>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                name: '',
+
+            }
+        },
+        computed: {
+            error(){
+                return this.name ? '' : 'Name is required Sohan!'
+
+            }
+
+        }
+
+    }
+
+</script>
