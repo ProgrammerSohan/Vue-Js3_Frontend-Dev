3d36b54de2607d6541be3fd74f3aa7757a51fff3
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-09 06:54:54 +0600

    2.4_2.01_implementing computed data into a vue component

--- s2_ch2/second-app-ch2/src/App.vue
+++ s2_ch2/second-app-ch2/src/App.vue
@@ -5,18 +5,23 @@
   <Computedproperty0 />
   <br>
   <Computedproperty1 />
+  <br> 
+  
+  <Exercise2a />
 
 </template>
 <script>
   import Test1 from './components/Test1-01.vue'
   import Computedproperty0 from './components/Computed-property0.vue'
   import Computedproperty1 from './components/Computed-property1.vue'
+  import Exercise2a from './components/Exercise2-01.vue'
 
 export default{
   components: {
     Test1,
     Computedproperty0,
-    Computedproperty1
+    Computedproperty1,
+    Exercise2a
   }
   
 
---
+++ s2_ch2/second-app-ch2/src/components/Exercise2-01.vue
@@ -0,0 +1,31 @@
+<template lang="">
+    <div>
+        <br>
+        <br>
+        <h1>Implementing Computed Data</h1>
+        <input v-model="firstName" placeholder="First Name">
+        <input v-model="lastName" placeholder="Last Name">
+        <h3 class="output">{{fullName}}</h3>
+        
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            firstName: '',
+            lastName: '',
+        }
+
+    },
+    computed: {
+        fullName(){
+            return `${this.firstName} ${this.lastName}`;
+
+        },
+
+    },
+
+}
+
+</script>
