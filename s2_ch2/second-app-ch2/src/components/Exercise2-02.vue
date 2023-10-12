243ab3ab1cedaa144f427eae6c93938466c3d912
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-12 21:25:31 +0600

    2.02_using computed setters_divideByTwo(b)

--- s2_ch2/second-app-ch2/src/components/Exercise2-02.vue
+++ s2_ch2/second-app-ch2/src/components/Exercise2-02.vue
@@ -3,6 +3,7 @@
         <h1>Using computed setters</h1>
         <input type="number" v-model="incrementOne">
         <h3>Get input: {{ incrementOne }}</h3>
+        <h3>Set division: {{ divideByTwo }}</h3>
     </div>
 </template>
 <script>
@@ -11,6 +12,7 @@
         data(){
             return {
                 count: -1,
+                divideByTwo: 0,
             }
         },
         computed:{
@@ -24,6 +26,7 @@
                 set(val){
 
                     this.count = val - 1
+                    this.divideByTwo = val /2
                 },
 
             },
